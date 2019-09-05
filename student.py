df810bdf2f565821101acde33fcdf0ff0fc5343e
Amogh Bagur <47712976+amoghbagur@users.noreply.github.com>
2019-09-05 18:31:52 +0530

    Update student.py

--- student.py
+++ student.py
@@ -5,7 +5,7 @@ i=0
 j=0
 k=0
 for i in stud:
-print('key is',i,'value is',stud[i])
+ print('key is',i,'value is',stud[i])
 list1[j]=stud[i]
 list2[k]=i
 j=j+1
@@ -14,4 +14,4 @@ print( 5 in list1)
 print(list2)
 print(stud.keys())
 print(stud.values())
-print(stud.items()
+print(stud.items())
