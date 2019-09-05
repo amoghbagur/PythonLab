2d30aedda8cae1078f5d1388b0941034cce7f0fe
Amogh Bagur <47712976+amoghbagur@users.noreply.github.com>
2019-09-05 18:08:43 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,17 @@
+class Person:
+def __init__(self,name,age):
+    self.name
+=name;
+    self.age=age;
+
+p1=Person("Arizona",14)
+p2=Person("Texas",12)
+
+print("\n Name of person #1 is",p1.name)
+print("\n Age of person #1 is",p1.age)
+
+print("\n Name of person #2 is",p2.name)
+print("\n Age of person #1 is",p2.age)
+
+p2.age=10
+print("\n Age of person #1 is",p2.age)
